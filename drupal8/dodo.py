a518daa7e2e8b8fd77454b89e7be013e75cdcb0c
Larry Garfield <larry@platform.sh>
2018-01-26 09:54:42 -0600

    Rework dodo file.

--- drupal8/dodo.py
+++ drupal8/dodo.py
@@ -6,23 +6,34 @@ DOIT_CONFIG = {
     "verbosity": 2,
 }
 
-
 def task_drupal8():
-
-    for task in common_tasks('drupal8'):
-        yield task
-
-    yield {
-        'basename': 'drupal8:init',
-        'file_dep': ['drupal8:clean'],
+    return {
+        'file_dep': ['drupal8_cleanup', 'drupal8_init', 'drupal8_update', 'drupal8_platformify', 'drupal8_branch'],
         'actions': [
             'git clone git@github.com:platformsh/template-drupal8.git template',
             'cd template && git remote add upstream https://github.com/drupal-composer/drupal-project.git'
         ]
     }
 
-    yield {
-        'basename': 'drupal8:update',
+def task_drupal8_cleanup():
+    return {
+        'actions': [
+            'rm -rf template'
+        ]
+    }
+
+def task_drupal8_init():
+    return {
+        'file_dep': ['drupal8_clean'],
+        'actions': [
+            'git clone git@github.com:platformsh/template-drupal8.git template',
+            'cd template && git remote add upstream https://github.com/drupal-composer/drupal-project.git'
+        ]
+    }
+
+
+def task_drupal8_update():
+    return {
         'actions': [
             'cd template && git fetch --all --depth=2',
             'cd template && git merge --allow-unrelated-histories -X theirs --squash upstream/8.x',
@@ -30,28 +41,25 @@ def task_drupal8():
         ]
     }
 
-    yield {
-        'basename': 'drupal8:platformify',
+def task_drupal8_platformify():
+    return {
         'actions': [
             'rsync -aP files/ template/'
         ]
     }
 
-def common_tasks(system):
-
-    yield {
-        'name': "%s:clean" % system,
-        'actions': [
-            'rm -rf template'
-        ]
-    }
-
-"""
+def drupal8_branch():
     return {
-        'basename': 'Drupal8',
         'actions': [
-            'echo hello',
-            'echo world'
+            'cd template && git checkout -b update',
+            'cd template && git add -A && git commit -m "Update to latest upstream"'
         ]
     }
-"""
+
+def drupal8_push():
+    return {
+        'actions': [
+            'cd template && git checkout update && git push -u origin update',
+            'cd template && hub pull-request -m "Update to latest upstream" -b master -h update'
+        ]
+    }

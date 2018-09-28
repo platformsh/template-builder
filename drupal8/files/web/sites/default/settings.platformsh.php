b9a1f7b94f1be2365f91d960e999cccf7cbf827a
Larry Garfield <larry@platform.sh>
2018-09-28 12:16:20 -0500

    Resync Drupal 8 template with example repo.

--- drupal8/files/web/sites/default/settings.platformsh.php
+++ drupal8/files/web/sites/default/settings.platformsh.php
@@ -5,8 +5,8 @@
  */
 
 // Configure the database.
-if (isset($_ENV['PLATFORM_RELATIONSHIPS'])) {
-  $relationships = json_decode(base64_decode($_ENV['PLATFORM_RELATIONSHIPS']), TRUE);
+if (getenv('PLATFORM_RELATIONSHIPS')) {
+  $relationships = json_decode(base64_decode(getenv('PLATFORM_RELATIONSHIPS')), TRUE);
   if (empty($databases['default']) && !empty($relationships)) {
     foreach ($relationships as $key => $relationship) {
       $drupal_key = ($key === 'database') ? 'default' : $key;
@@ -38,14 +38,14 @@ if (isset($_ENV['PLATFORM_RELATIONSHIPS'])) {
   }
 }
 
-if (isset($_ENV['PLATFORM_APP_DIR'])) {
+if (getenv('PLATFORM_APP_DIR')) {
 
   // Configure private and temporary file paths.
   if (!isset($settings['file_private_path'])) {
-    $settings['file_private_path'] = $_ENV['PLATFORM_APP_DIR'] . '/private';
+    $settings['file_private_path'] = getenv('PLATFORM_APP_DIR') . '/private';
   }
   if (!isset($config['system.file']['path']['temporary'])) {
-    $config['system.file']['path']['temporary'] = $_ENV['PLATFORM_APP_DIR'] . '/tmp';
+    $config['system.file']['path']['temporary'] = getenv('PLATFORM_APP_DIR') . '/tmp';
   }
 
   // Configure the default PhpStorage and Twig template cache directories.
@@ -59,12 +59,12 @@ if (isset($_ENV['PLATFORM_APP_DIR'])) {
 }
 
 // Set trusted hosts based on Platform.sh routes.
-if (isset($_ENV['PLATFORM_ROUTES']) && !isset($settings['trusted_host_patterns'])) {
-  $routes = json_decode(base64_decode($_ENV['PLATFORM_ROUTES']), TRUE);
+if (getenv('PLATFORM_ROUTES') && !isset($settings['trusted_host_patterns'])) {
+  $routes = json_decode(base64_decode(getenv('PLATFORM_ROUTES')), TRUE);
   $settings['trusted_host_patterns'] = [];
   foreach ($routes as $url => $route) {
     $host = parse_url($url, PHP_URL_HOST);
-    if ($host !== FALSE && $route['type'] == 'upstream' && $route['upstream'] == $_ENV['PLATFORM_APPLICATION_NAME']) {
+    if ($host !== FALSE && $route['type'] == 'upstream' && $route['upstream'] == getenv('PLATFORM_APPLICATION_NAME')) {
       // Replace asterisk wildcards with a regular expression.
       $host_pattern = str_replace('\*', '[^\.]+', preg_quote($host));
       $settings['trusted_host_patterns'][] = '^' . $host_pattern . '$';
@@ -75,40 +75,53 @@ if (isset($_ENV['PLATFORM_ROUTES']) && !isset($settings['trusted_host_patterns']
 
 // Import variables prefixed with 'd8settings:' into $settings and 'd8config:'
 // into $config.
-if (isset($_ENV['PLATFORM_VARIABLES'])) {
-  $variables = json_decode(base64_decode($_ENV['PLATFORM_VARIABLES']), TRUE);
+if (getenv('PLATFORM_VARIABLES')) {
+  $variables = json_decode(base64_decode(getenv('PLATFORM_VARIABLES')), TRUE);
   foreach ($variables as $name => $value) {
-    // A variable named "d8settings:example-setting" will be saved in
-    // $settings['example-setting'].
-    if (strpos($name, 'd8settings:') === 0) {
-      $settings[substr($name, 11)] = $value;
-    }
-    // A variable named "drupal:example-setting" will be saved in
-    // $settings['example-setting'] (backwards compatibility).
-    elseif (strpos($name, 'drupal:') === 0) {
-      $settings[substr($name, 7)] = $value;
-    }
-    // A variable named "d8config:example-name:example-key" will be saved in
-    // $config['example-name']['example-key'].
-    elseif (strpos($name, 'd8config:') === 0 && substr_count($name, ':') >= 2) {
-      list(, $config_key, $config_name) = explode(':', $name, 3);
-      $config[$config_key][$config_name] = $value;
-    }
-    // A complex variable named "d8config:example-name" will be saved in
-    // $config['example-name'].
-    elseif (strpos($name, 'd8config:') === 0 && is_array($value)) {
-      $config[substr($name, 9)] = $value;
+    $parts = explode(':', $name);
+    list($prefix, $key) = array_pad($parts, 3, null);
+    switch ($prefix) {
+      // Variables that begin with `d8settings` or `drupal` get mapped
+      // to the $settings array verbatim, even if the value is an array.
+      // For example, a variable named d8settings:example-setting' with
+      // value 'foo' becomes $settings['example-setting'] = 'foo';
+      case 'd8settings':
+      case 'drupal':
+        $settings[$key] = $value;
+        break;
+      // Variables that begin with `d8config` get mapped to the $config
+      // array.  Deeply nested variable names, with colon delimiters,
+      // get mapped to deeply nested array elements. Array values
+      // get added to the end just like a scalar. Variables without
+      // both a config object name and property are skipped.
+      // Example: Variable `d8config:conf_file:prop` with value `foo` becomes
+      // $config['conf_file']['prop'] = 'foo';
+      // Example: Variable `d8config:conf_file:prop:subprop` with value `foo` becomes
+      // $config['conf_file']['prop']['subprop'] = 'foo';
+      // Example: Variable `d8config:conf_file:prop:subprop` with value ['foo' => 'bar'] becomes
+      // $config['conf_file']['prop']['subprop']['foo'] = 'bar';
+      // Example: Variable `d8config:prop` is ignored.
+      case 'd8config':
+        if (count($parts) > 2) {
+          $temp = &$config[$key];
+          foreach (array_slice($parts, 2) as $n) {
+            $prev = &$temp;
+            $temp = &$temp[$n];
+          }
+          $prev[$n] = $value;
+        }
+        break;
     }
   }
 }
 
 // Set the project-specific entropy value, used for generating one-time
 // keys and such.
-if (isset($_ENV['PLATFORM_PROJECT_ENTROPY']) && empty($settings['hash_salt'])) {
-  $settings['hash_salt'] = $_ENV['PLATFORM_PROJECT_ENTROPY'];
+if (getenv('PLATFORM_PROJECT_ENTROPY') && empty($settings['hash_salt'])) {
+  $settings['hash_salt'] = getenv('PLATFORM_PROJECT_ENTROPY');
 }
 
 // Set the deployment identifier, which is used by some Drupal cache systems.
-if (isset($_ENV['PLATFORM_TREE_ID']) && empty($settings['deployment_identifier'])) {
-  $settings['deployment_identifier'] = $_ENV['PLATFORM_TREE_ID'];
+if (getenv('PLATFORM_TREE_ID') && empty($settings['deployment_identifier'])) {
+  $settings['deployment_identifier'] = getenv('PLATFORM_TREE_ID');
 }

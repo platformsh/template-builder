f6f63bd53ebcb9d3c3ef72e857ae80f9680e6af0
Larry Garfield <larry@platform.sh>
2018-12-20 16:13:16 -0600

    Add MySQL sample file for the PHP template.

--- php/files/web/index.php
+++ php/files/web/index.php
@@ -8,3 +8,12 @@ $messages = new Messages();
 
 printf("<h1>%s</h1>\n", $messages->title());
 printf("<p>%s</p>\n", $messages->message());
+
+
+$files = glob("../examples/*.php");
+foreach ($files as $filename) {
+    print "<details>\n";
+    include $filename;
+    print "</details>\n\n";
+}
+

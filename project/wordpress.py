711e4ace3689371cbdc823396542ad50618b30b6
Larry Garfield <larry@platform.sh>
2019-01-10 11:40:22 -0600

    Update to latest WP version.

--- project/wordpress.py
+++ project/wordpress.py
@@ -5,7 +5,7 @@ from .remote import RemoteProject
 
 
 class Wordpress(RemoteProject):
-    upstream_tag = '5.0.1'
+    upstream_tag = '5.0.3'
     remote = 'https://github.com/johnpbloch/wordpress.git'
 
     @property

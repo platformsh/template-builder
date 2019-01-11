16b503afb083325e252271ae5f061c9c82cd679c
Larry Garfield <larry@platform.sh>
2019-01-11 11:20:12 -0600

    Fix Symfony 3 Platform version.

--- project/symfony.py
+++ project/symfony.py
@@ -5,6 +5,17 @@ class Symfony3(RemoteProject):
     upstream_branch = '3.4'
     remote = 'https://github.com/symfony/symfony-standard.git'
 
+    @property
+    def platformify(self):
+        return super(Symfony3, self).platformify + [
+            # Symfony 3 ships with a composer.json file that specifies a platform target
+            # Of PHP 5.6, for no good reason, even though it depends on Doctrine, which
+            # requires PHP 7.1.  Since we're guaranteeing a PHP 7.2 environment, just
+            # change that requirement to 7.2 and be done with it.
+            'cd {0} && composer config platform.php 7.2'.format(
+                self.builddir)
+        ]
+
 
 class Symfony4(RemoteProject):
     upstream_branch = '4.2'

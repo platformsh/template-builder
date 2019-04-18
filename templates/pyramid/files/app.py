73a92377892711031dd2d71e042cf7c67426e867
chadcarlson <chad.carlson@platform.sh>
2019-04-18 15:09:12 -0400

    add python pyramid

---
+++ templates/pyramid/files/app.py
@@ -0,0 +1,16 @@
+import os
+from wsgiref.simple_server import make_server
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+
+def hello_world(request):
+    return Response('Hello World!')
+
+if __name__ == '__main__':
+    with Configurator() as config:
+        config.add_route('hello', '/')
+        config.add_view(hello_world, route_name='hello')
+        app = config.make_wsgi_app()
+    server = make_server('127.0.0.1', int(os.environ["PORT"]), app)
+    server.serve_forever()

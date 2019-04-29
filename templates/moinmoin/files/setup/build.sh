e48919a8b5d4ada354acfb74ef396cf5d80b67ff
chadcarlson <chad.carlson@platform.sh>
2019-04-29 13:25:54 -0400

    Move doc root to moinmoin subdir

--- templates/moinmoin/files/setup/build.sh
+++ templates/moinmoin/files/setup/build.sh
@@ -5,23 +5,21 @@ MOINVERSION=1.9.10
 # Download MoinMoin.
 curl -o moin-install.tar.gz http://static.moinmo.in/files/moin-$MOINVERSION.tar.gz
 tar xvzf moin-install.tar.gz
-mv moin-$MOINVERSION moin-install
+mv moin-$MOINVERSION moinmoin
+rm moin-install.tar.gz
 
 # Apply patches.
-patch moin-install/MoinMoin/logfile/editlog.py ~/setup/editlog.patch
-patch moin-install/MoinMoin/logfile/eventlog.py ~/setup/eventlog.patch
+patch moinmoin/MoinMoin/logfile/editlog.py setup/editlog.patch
+patch moinmoin/MoinMoin/logfile/eventlog.py setup/eventlog.patch
 
-# Move install to app_dir.
-mv ~/moin-install/* /app/
-
-# Remove the install.
-rm -rf ~/moin-install
-rm ~/moin-install.tar.gz
+# Move local configuration to the install.
+mv wikiconfig_local.py moinmoin
+mv wikiserverconfig_local.py moinmoin
 
 # Stage files that will go to mounts on first deploy.
-mkdir ~/setup/temp
-mv ~/wiki/data/pages ~/setup/temp
-mv ~/wiki/data/cache ~/setup/temp
-mv ~/wiki/data/edit-log ~/setup/temp
-mv ~/wiki/data/event-log ~/setup/temp
-mv ~/wiki/underlay ~/setup/temp
+mkdir setup/temp
+mv moinmoin/wiki/data/pages setup/temp
+mv moinmoin/wiki/data/cache setup/temp
+mv moinmoin/wiki/data/edit-log setup/temp
+mv moinmoin/wiki/data/event-log setup/temp
+mv moinmoin/wiki/underlay setup/temp
--- templates/moinmoin/files/setup/deploy.sh
+++ templates/moinmoin/files/setup/deploy.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-if [ ! -f wiki/underlay/moin.installed ]; then
+if [ ! -f moinmoin/wiki/underlay/moin.installed ]; then
 
     # Copy staged files to mounts.
-    cp -a ~/setup/temp/pages/* ~/wiki/data/pages
-    cp -a ~/setup/temp/cache/* ~/wiki/data/cache
-    cp -a ~/setup/temp/underlay/* ~/wiki/underlay
-    cp -a ~/setup/temp/edit-log ~/wiki/data/logging
-    cp -a ~/setup/temp/event-log ~/wiki/data/logging
+    cp -a setup/temp/pages/* moinmoin/wiki/data/pages
+    cp -a setup/temp/cache/* moinmoin/wiki/data/cache
+    cp -a setup/temp/underlay/* moinmoin/wiki/underlay
+    cp -a setup/temp/edit-log moinmoin/wiki/data/logging
+    cp -a setup/temp/event-log moinmoin/wiki/data/logging
 
-    touch wiki/underlay/moin.installed
+    touch moinmoin/wiki/underlay/moin.installed
 fi

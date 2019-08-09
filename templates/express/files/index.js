8e2debf4a7687c06108b3fcdb5a756f55f23513f
chadcarlson <chad.carlson@platform.sh>
2019-08-09 14:51:45 -0400

    Express prepared statements updates.

--- templates/express/files/index.js
+++ templates/express/files/index.js
@@ -5,100 +5,70 @@ const config = require("platformsh-config").config();
 // Define the application.
 var app = express();
 
-async function mariadbTest() {
-    try {
+// Connect to MariaDB.
+async function openConnection () {
+  const credentials = config.credentials('database');
+  return await mysql.createConnection({
+      host: credentials.host,
+      port: credentials.port,
+      user: credentials.username,
+      password: credentials.password,
+      database: credentials.path,
+  });
+};
 
-        var results = [];
-
-        // Connect to MariaDB.
-        const credentials = config.credentials('database');
-        const connection = await mysql.createConnection({
-            host: credentials.host,
-            port: credentials.port,
-            user: credentials.username,
-            password: credentials.password,
-            database: credentials.path,
-        });
-
-        results[0] = `Connected to MariaDB at ${credentials.host}.`
-
-        // Add the table.
-        tableName = "userinfo";
-
-        let sql = '';
-
-        // Create a table.
-        sql = `CREATE TABLE IF NOT EXISTS userinfo (
-          uid INT(10) NOT NULL AUTO_INCREMENT,
-          username VARCHAR(64) NULL DEFAULT NULL,
-          departname VARCHAR(128) NULL DEFAULT NULL,
-          created DATE NULL DEFAULT NULL,
-          PRIMARY KEY (uid)
-        ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;`;
-
-        await connection.query(sql);
-
-        results[1] = "Created table 'userinfo'.";
-
-        // Insert data.
-        values = `(username, departname, created)`;
-        newRow = `('platform', 'Deploy Friday', '2019-06-17')`;
-
-        sql = "INSERT INTO userinfo (username, departname, created) VALUES ('platform', 'Deploy Friday', '2019-06-17')"
-
-        await connection.query(sql);
-
-        // Read the data.
-        sql = "SELECT * FROM userinfo";
-        let [rows] = await connection.query(sql);
-
-        let output = '';
-
-        if (rows.length > 0) {
-          rows.forEach((row) => {
-            output += `  - uid (1): ${row.uid}
-  - username (platform): ${row.username}
-  - departname (Deploy Friday): ${row.departname}
-  - created (2019-06-17): ${row.created}`;
-          });
-        }
-
-        results[2] = output;
-
-        // Drop the table.
-        sql = "DROP TABLE userinfo";
-        await connection.query(sql);
-
-        return results;
-
-      } catch(error) {
-        return Promise.reject(error);
-      }
-}
+// Drop the table and retrieve result information.
+function dropTable (connection) {
+  let results = connection.execute("DROP TABLE platforminfo")
+  .then((result) => {
+    return result;
+  })
+  return results
+};
 
 // Define the main route.
 app.get('/', async function(req, res){
 
-  let results = await mariadbTest();
+  // Connect to MariaDB.
+  let connection = await openConnection();
 
-  outputString = `Hello, World! - A simple Express web framework template for Platform.sh
+  // Create the table.
+  connection.execute(
+    `CREATE TABLE IF NOT EXISTS platforminfo (
+      uid INT(10) NOT NULL AUTO_INCREMENT,
+      username VARCHAR(64) NULL DEFAULT NULL,
+      departname VARCHAR(128) NULL DEFAULT NULL,
+      created DATE NULL DEFAULT NULL,
+      PRIMARY KEY (uid)
+    ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;`
+  );
 
+  // Insert data.
+  connection.execute(
+    "INSERT INTO platforminfo (username, departname, created) VALUES ('platform', 'Deploy Friday', '2019-06-17')"
+  );
+
+  // Read the data.
+  let [rows] = await connection.query("SELECT * FROM platforminfo");
+
+  // Drop the table.
+  let dropped_result = await dropTable(connection);
+
+  // Make the output.
+  var outputString = `Hello, World! - A simple Express web framework template for Platform.sh
 
 MariaDB Tests:
 
-* Connect:
-  - ${results[0]}
-
-* Add table:
-  - ${results[1]}
-
-* Add row to table and verify:
-${results[2]}
-`;
+* Connect and add row:
+  - Row ID (1): ${rows[0].uid}
+  - Username (platform): ${rows[0].username}
+  - Department (Deploy Friday): ${rows[0].departname}
+  - Created (2019-06-17): ${rows[0].created}
+* Delete row:
+  - Status (0): ${dropped_result[0].warningStatus}`;
 
   res.set('Content-Type', 'text/plain');
   res.send(outputString);
-
 
 });
 

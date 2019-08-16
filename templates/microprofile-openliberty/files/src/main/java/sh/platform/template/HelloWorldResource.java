76cce8d3edabb71ca20740da87417c43b8f65e1c
Otavio Santana <otaviopolianasantana@gmail.com>
2019-08-16 16:33:31 -0500

    fixes openliberty

--- templates/microprofile-openliberty/files/src/main/java/sh/platform/template/HelloWorldResource.java
+++ templates/microprofile-openliberty/files/src/main/java/sh/platform/template/HelloWorldResource.java
@@ -12,6 +12,6 @@ public class HelloWorldResource {
     @GET
     @Produces("text/plain")
     public String doGet() {
-        return Hello world - a simple Microprofile Open Liberty template for Platform.sh;
+        return "Hello world - a simple Microprofile Open Liberty template for Platform.sh";
     }
 }

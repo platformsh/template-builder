c784420f28839a9d1047bc98e872e37f24314d5e
Larry Garfield <larry@platform.sh>
2019-09-09 12:47:27 -0500

    Revise multi-site handling to be more streamlined.

--- templates/drupal8multi/files/web/sites/second/settings.php
+++ templates/drupal8multi/files/web/sites/second/settings.php
@@ -27,16 +27,18 @@ $settings['file_scan_ignore_directories'] = [
 // local development.
 // $settings['hash_salt'] = 'change_me';
 
+$platformsh_subsite_id = basename(__DIR__);
+
+// Set this to true to enable Redis caching, but only after the site has been installed.
+$platformsh_enable_redis = false;
+
 // Set up a config sync directory.
 //
 // This is defined inside the read-only "config" directory, deployed via Git.
-$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync';
+$config_directories[CONFIG_SYNC_DIRECTORY] = '../config/sync/' . $platformsh_subsite_id;
 
 // Automatic Platform.sh settings.
 if (file_exists($app_root . '/' . $site_path . '/../settings.platformsh.php')) {
-  if (file_exists("{$app_root}/{$site_path}/site-definition.php")) {
-    include "{$app_root}/{$site_path}/site-definition.php";
-  }
   include $app_root . '/' . $site_path . '/../settings.platformsh.php';
 }
 

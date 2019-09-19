4c5294411fdf81e08f0e8ab9afc2f89be8e4b9da
Otávio Santana <otaviopolianasantana@gmail.com>
2019-09-19 10:31:21 -0700

    Update templates/jetty/files/src/main/java/sh/platform/template/HelloWorldServlet.java
    Co-Authored-By: Chad Carlson <chadwcarlson@users.noreply.github.com>

--- templates/jetty/files/src/main/java/sh/platform/template/HelloWorldServlet.java
+++ templates/jetty/files/src/main/java/sh/platform/template/HelloWorldServlet.java
@@ -11,6 +11,6 @@ public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.getWriter().println("Hello World! from Platform.sh");
+        response.getWriter().println("Hello World! A simple Jetty template for Platform.sh");
     }
 }

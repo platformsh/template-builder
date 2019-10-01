3d99ab588d2e8667db9691c21e61a5aff492bccd
Larry Garfield <larry@platform.sh>
2019-10-01 12:02:00 -0500

    Strip the Ruby version from the Gemfile as it locks too tightly.

--- project/rails.py
+++ project/rails.py
@@ -16,5 +16,8 @@ class Rails(BaseProject):
             'cd {0} && bundle add unicorn pg platform_sh_rails --group "production" --skip-install'.format(self.builddir),
             'cd {0} && echo config/database.yml >> .gitignore'.format(self.builddir),
             'cd {0} && mv config/database.yml config/database.yml.example'.format(self.builddir),
+            # Remove the Rails Ruby version lock file, as it's not needed.
             'cd {0} && rm .ruby-version'.format(self.builddir),
+            # Remove the Ruby version from the Gemfile, as it pins to a .z release which is too strict.
+            "cd {0} && sed '/^ruby /d' Gemfile > temp && mv temp Gemfile".format(self.builddir),
         ]

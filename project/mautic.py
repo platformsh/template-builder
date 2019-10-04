0c64b1fbb266ed42d3c7cefd8ef1cd7bf63c3f8c
Larry Garfield <larry@platform.sh>
2019-10-04 10:49:26 -0500

    Force Mautic to use Twig 1.

--- project/mautic.py
+++ project/mautic.py
@@ -5,9 +5,16 @@ class Mautic(RemoteProject):
     major_version = '2'
     remote = 'https://github.com/mautic/mautic.git'
 
+
     @property
-    def platformify(self):
-        return super(Mautic, self).platformify + [
-            'cd {0} && composer remove friendsofphp/php-cs-fixer --ignore-platform-reqs'.format(
-                self.builddir)
-        ]
+    def update(self):
+        actions = super(Mautic, self).update
+
+        # composer update will pull in Twig 2 instead of Twig 1, which is declared
+        # compatible according to the composer.json files but really isn't. It will crash
+        # when compiling templates. So force Twig 1 here, before the first composer update
+        # runs.
+        actions.insert(4, 'cd {0} && composer require twig/twig ~1.42 --no-scripts --ignore-platform-reqs'.format(
+            self.builddir))
+
+        return actions

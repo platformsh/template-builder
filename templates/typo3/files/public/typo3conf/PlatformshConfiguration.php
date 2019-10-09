c00e70cdc8992eebeb703f4579cbcbd627322c4f
Larry Garfield <larry@platform.sh>
2019-10-09 18:21:41 -0500

    Split Platformsh glue to its own file.

---
+++ templates/typo3/files/public/typo3conf/PlatformshConfiguration.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+use Platformsh\ConfigReader\Config;
+
+$platformConfig = new Config();
+
+if (!$platformConfig->isValidPlatform()) {
+    return;
+}
+
+if ($platformConfig->inBuild()) {
+    return;
+}
+
+// Workaround to set the proper env variable for the main route (found in config/sites/main/config.yaml)
+putenv('PLATFORM_ROUTES_MAIN=' . $platformConfig->getRoute('main')['url']);
+$databaseConfig = $platformConfig->credentials('database');
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['driver'] = 'mysqli';
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['host'] = $databaseConfig['host'];
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['port'] = $databaseConfig['port'];
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['dbname'] = $databaseConfig['path'];
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['user'] = $databaseConfig['username'];
+$GLOBALS['TYPO3_CONF_VARS']['DB']['Connections']['Default']['password'] = $databaseConfig['password'];
+$redisConfig = $platformConfig->credentials('rediscache');
+$redisHost = $redisConfig['host'];
+$redisPort = $redisConfig['port'];
+$list = [
+    'pages' => 3600 * 24 * 7,
+    'pagesection' => 3600 * 24 * 7,
+    'rootline' => 3600 * 24 * 7,
+    'hash' => 3600 * 24 * 7,
+    'extbase' => 3600 * 24 * 7,
+];
+$counter = 1;
+foreach ($list as $key => $lifetime) {
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$key]['backend'] = \TYPO3\CMS\Core\Cache\Backend\RedisBackend::class;
+    $GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$key]['options'] = [
+        'database' => $counter++,
+        'hostname' => $redisHost,
+        'port' => $redisPort,
+        'defaultLifetime' => $lifetime
+    ];
+}

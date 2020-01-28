5a64ac454e6bc6f18347530c30d2330e4bebd95c
Nick Anderegg <nick@anderegg.io>
2020-01-28 17:10:46 -0500

    Change `Drupal7Vanilla` class to `Drupal7_vanilla`

--- project/templates/drupal.py
+++ project/templates/drupal.py
@@ -4,13 +4,15 @@ from collections import OrderedDict
 from project import BaseProject, RemoteProject
 
 
-class Drupal7Vanilla(BaseProject):
-    version = '7.67'
+class Drupal7_vanilla(BaseProject):
+    version = "7.67"
 
     @property
     def update(self):
-        return super(Drupal7Vanilla, self).update + [
-            "wget https://ftp.drupal.org/files/projects/drupal-{0}.tar.gz && tar xzvf drupal-{0}.tar.gz -C {1}".format(self.version, self.builddir),
+        return super(Drupal7_vanilla, self).update + [
+            "wget https://ftp.drupal.org/files/projects/drupal-{0}.tar.gz && tar xzvf drupal-{0}.tar.gz -C {1}".format(
+                self.version, self.builddir
+            ),
             "rm drupal-{0}.tar.gz".format(self.version),
             "rm -rf {0}public || true".format(self.builddir),
             "mv {0}drupal-{1} {0}public".format(self.builddir, self.version),
@@ -18,28 +20,30 @@ class Drupal7Vanilla(BaseProject):
 
 
 class Drupal8(RemoteProject):
-    major_version = '8.8'
-    remote = 'https://github.com/drupal/recommended-project.git'
+    major_version = "8.8"
+    remote = "https://github.com/drupal/recommended-project.git"
 
     @property
     def platformify(self):
         return super(Drupal8, self).platformify + [
-            'cd {0} && composer require platformsh/config-reader drush/drush drupal/console drupal/redis --ignore-platform-reqs'.format(
-                self.builddir)
+            "cd {0} && composer require platformsh/config-reader drush/drush drupal/console drupal/redis --ignore-platform-reqs".format(
+                self.builddir
+            )
         ]
+
 
 class Drupal8_multisite(Drupal8):
     pass
 
+
 class Drupal8_opigno(Drupal8):
-    major_version = '2'
-    remote = 'https://bitbucket.org/opigno/opigno-composer.git'
+    major_version = "2"
+    remote = "https://bitbucket.org/opigno/opigno-composer.git"
 
 
 class Drupal8_govcms8(Drupal8):
-    major_version = '8.x'
-    remote = 'https://github.com/govCMS/govCMS8.git'
-
+    major_version = "8.x"
+    remote = "https://github.com/govCMS/govCMS8.git"
 
     @property
     def platformify(self):
@@ -49,8 +53,10 @@ class Drupal8_govcms8(Drupal8):
             within itself.  It's not clear why, but it's unnecessary. Skip that.
             """
 
-            composer['scripts']['post-install-cmd'].remove('@composer push')
-            composer['scripts']['post-update-cmd'].remove('@composer push')
+            composer["scripts"]["post-install-cmd"].remove("@composer push")
+            composer["scripts"]["post-update-cmd"].remove("@composer push")
             return composer
 
-        return [(self.modify_composer, [govcms_remove_phing])] + super(Drupal8, self).platformify
+        return [(self.modify_composer, [govcms_remove_phing])] + super(
+            Drupal8, self
+        ).platformify

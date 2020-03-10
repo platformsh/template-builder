2848f3e57f0c2b806206466d5842f20f29849e38
Larry Garfield <larry@platform.sh>
2020-03-10 15:27:00 -0500

    Use Laravel 7.

--- project/laravel.py
+++ project/laravel.py
@@ -2,12 +2,11 @@ from .remote import RemoteProject
 
 
 class Laravel(RemoteProject):
-    major_version = 'v5'
+    major_version = 'v7'
     remote = 'https://github.com/laravel/laravel.git'
 
     @property
     def platformify(self):
         return super(Laravel, self).platformify + [
             'cd {0} && composer require platformsh/laravel-bridge'.format(self.builddir),
-            'cd {0} && rm readme.md'.format(self.builddir),
         ]

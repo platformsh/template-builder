a4120b5b974c5575a50825008f5b6edc29097336
chadcarlson <chad.carlson@platform.sh>
2020-03-18 20:15:10 -0400

    Not sure why this is necessary when including the include, but it is.

--- templates/mattermost/files/setup.sh
+++ templates/mattermost/files/setup.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+chmod +x start.sh
+chmod +x bin/mattermost
+
 MATTERMOST_VERSION=5.21.0
 
 wget https://releases.mattermost.com/${MATTERMOST_VERSION}/mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz

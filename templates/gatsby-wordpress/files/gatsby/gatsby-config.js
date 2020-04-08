764ec9616864e060b745c74d02b88fc6cf5157b0
chadcarlson <chad.carlson@platform.sh>
2020-04-08 12:51:13 -0400

    Fix routes. Use internal.

--- templates/gatsby-wordpress/files/gatsby/gatsby-config.js
+++ templates/gatsby-wordpress/files/gatsby/gatsby-config.js
@@ -19,7 +19,7 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'demo.wp-api.org' or 'www.example-site.com'
          */
-        baseUrl: config.getRoute("wordpress").url.split("/")[2],
+        baseUrl: config.credentials("wordpress").url.split("/")[2],
         // The protocol. This can be http or https.
         protocol: `https`,
         // Indicates whether the site is hosted on wordpress.com.

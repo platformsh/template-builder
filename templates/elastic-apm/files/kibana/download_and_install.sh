e661a953287012da893fdbf48475e12734e6f493
Larry Garfield <larry@platform.sh>
2020-04-15 18:13:56 -0500

    Whitespace.

--- templates/elastic-apm/files/kibana/download_and_install.sh
+++ templates/elastic-apm/files/kibana/download_and_install.sh
@@ -1,5 +1,5 @@
 wget https://artifacts.elastic.co/downloads/kibana/kibana-$KIBANA_VERSION-$KIBANA_PLATFORM.tar.gz
-shasum -a 512 kibana-$KIBANA_VERSION-$KIBANA_PLATFORM.tar.gz 
+shasum -a 512 kibana-$KIBANA_VERSION-$KIBANA_PLATFORM.tar.gz
 tar -xzf kibana-$KIBANA_VERSION-$KIBANA_PLATFORM.tar.gz
 rm kibana-$KIBANA_VERSION-$KIBANA_PLATFORM/config/kibana.yml
 mv kibana-$KIBANA_VERSION-$KIBANA_PLATFORM kibana

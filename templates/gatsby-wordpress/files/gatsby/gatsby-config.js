5fc90e94983af66bd856ba7522cf7c3df81c4b6c
chadcarlson <chad.carlson@platform.sh>
2020-05-04 17:17:25 -0400

    Fixes baseURL and protocol.

--- templates/gatsby-wordpress/files/gatsby/gatsby-config.js
+++ templates/gatsby-wordpress/files/gatsby/gatsby-config.js
@@ -19,9 +19,9 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'demo.wp-api.org' or 'www.example-site.com'
          */
-        baseUrl: config.credentials("wordpress").url.split("/")[2],
+        baseUrl: config.credentials("wordpress")["host"],
         // The protocol. This can be http or https.
-        protocol: `https`,
+        protocol: `http`,
         // Indicates whether the site is hosted on wordpress.com.
         // If false, then the assumption is made that the site is self hosted.
         // If true, then the plugin will source its content on wordpress.com using the JSON REST API V2.

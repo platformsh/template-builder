0b8b45afe94523f232d98431e2612460bf1074ee
chadcarlson <chad.carlson@platform.sh>
2020-05-06 13:14:11 -0400

    Description feedback.

--- project/phoenix.py
+++ project/phoenix.py
@@ -4,7 +4,7 @@ import os
 
 class Phoenix_elixir(BaseProject):
     '''Phoenix_elixir object.
-    This is necessary because elixir has some very specific naming conventions, that prevent the template from being called
+    This is necessary because elixir has some very specific naming conventions that prevent the template from being called
         simply 'phoenix' (since it is trying to compile the phoenix Hex module), and the project cannot contain the '-' character,
     only underscore.
     This constructor sets the 'github_name' attribute to get around the naming convention checks in the BaseProject constructor.

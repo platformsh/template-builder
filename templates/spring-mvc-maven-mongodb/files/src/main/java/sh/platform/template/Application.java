a7af215a62619c5b8a7df6f2fe430427cf913d72
otaviojava <otaviopolianasantana@gmail.com>
2020-06-02 06:04:42 -0300

    updates Spring data 2.3.0

--- templates/spring-mvc-maven-mongodb/files/src/main/java/sh/platform/template/Application.java
+++ templates/spring-mvc-maven-mongodb/files/src/main/java/sh/platform/template/Application.java
@@ -2,10 +2,8 @@ package sh.platform.template;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 public class Application {
 
     public static void main(String[] args) {

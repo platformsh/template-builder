dd12aad9682e9e3b1968585b8f8ce0e2c6119c40
Larry Garfield <larry@platform.sh>
2020-06-03 16:58:24 -0500

    Break plugin installation out to a separate file for ease of use.

--- templates/discourse/files/build.sh
+++ templates/discourse/files/build.sh
@@ -4,23 +4,37 @@ mv config/discourse.platformsh.conf config/discourse.conf
 mv public/ _public/
 # create symbolic link form app/tmp to /tmp so Bootsnap doesn't die.
 ln -s /tmp tmp
-# install nvm
 
+# install nvm
 unset NPM_CONFIG_PREFIX
 curl -o- https://raw.githubusercontent.com/creationix/nvm/$NVM_VERSION/install.sh | dash
 export NVM_DIR="$PLATFORM_APP_DIR/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm current
 nvm install $NODE_VERSION
+
 # Install globally some more required npm packages that are not in package.json
 npm i -g "yarn" "svgo" "gifsicle@4.0.1" "uglify@<3"
+
 # install the version of bundler specified in Gemfile.lock (should not be necessary with ruby 2.7 and up. This is a Gem bug.)
 bundle update --bundler
 gem install bundler -v "$(grep -A 1 "BUNDLED WITH" Gemfile.lock | tail -n 1)"
+
 # Export the current ruby version
 RUBY_VERSION=$(ruby -e"puts RUBY_VERSION")
-# Install official plugins
-# To install plugins ... uncomment the following lines
+
+# Install plugins
+
+echo "Installing Additional Plugins"
+cd plugins
+file="../plugins.txt"
+while read -r line; do
+    [[ "$line" =~ ^#.*$ ]] && continue
+    git clone --depth=1 $line
+done < "$file"
+cd ..
+
+
 # git clone --depth=1 https://github.com/discourse/discourse-backup-uploads-to-s3.git plugins/discourse-backup-uploads-to-s3
 # git clone --depth=1 https://github.com/discourse/discourse-spoiler-alert.git plugins/discourse-spoiler-alert
 # git clone --depth=1 https://github.com/discourse/discourse-cakeday.git plugins/discourse-cakeday
@@ -30,8 +44,10 @@ RUBY_VERSION=$(ruby -e"puts RUBY_VERSION")
 # git clone --depth=1 https://github.com/discourse/discourse-patreon.git plugins/discourse-patreon
 # git clone --depth=1 https://github.com/discourse/discourse-user-notes.git plugins/discourse-user-notes
 # git clone --depth=1 https://github.com/discourse/discourse-group-tracker
+
 # Install Gems
 bundle install --retry 3 --jobs 4
 bundle exec rake plugin:install_all_gems
+
 # Optionally install the CLI
 # curl -sS https://platform.sh/cli/installer | php

2d93ffa78a315f79bd39cdd07f14cbba046149eb
Larry Garfield <larry@platform.sh>
2020-06-04 18:17:45 -0500

    Precompile in deploy the first time.

--- templates/discourse/files/deploy.sh
+++ templates/discourse/files/deploy.sh
@@ -8,6 +8,10 @@ if [ ! -f install/platform.installed ]; then
     # Copy the public files into a writeable directory before running the migration
     # on the initial install. After the first deploy this can be put off to post_deploy.
     rsync -avq --update --ignore-errors _public/ public/
+    # It's unclear why these need to be precompiled in deploy rather than post-deploy
+    # the first time the site is deployed, but they do.
+    bundle exec rake assets:precompile -q
     touch install/platform.installed
 fi
+
 bundle exec rake db:migrate -q

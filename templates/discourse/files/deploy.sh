a7121af52d5cdfa8f5b260150faeceda73cf7f28
Larry Garfield <larry@platform.sh>
2020-06-08 10:16:43 -0500

    Disable deploy/post-deploy changes for testing.

--- templates/discourse/files/deploy.sh
+++ templates/discourse/files/deploy.sh
@@ -4,7 +4,7 @@ unset NPM_CONFIG_PREFIX
 export NVM_DIR="$PLATFORM_APP_DIR/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use $NODE_VERSION
-if [ ! -f install/platform.installed ]; then
+#if [ ! -f install/platform.installed ]; then
     # Copy the public files into a writeable directory before running the migration
     # on the initial install. After the first deploy this can be put off to post_deploy.
     rsync -avq --update --ignore-errors _public/ public/
@@ -12,6 +12,6 @@ if [ ! -f install/platform.installed ]; then
     # the first time the site is deployed, but they do.
     bundle exec rake assets:precompile -q
     touch install/platform.installed
-fi
+#fi
 
 bundle exec rake db:migrate -q

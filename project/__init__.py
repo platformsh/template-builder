62529b4a295cfeea7bfa66dedaee9160fd1e8115
Ludovico Altana <ludovico@platform.sh>
2020-06-10 15:03:35 +0200

    Add steps to create, test and merge pull requests

--- project/__init__.py
+++ project/__init__.py
@@ -3,10 +3,11 @@ import os.path
 import json
 from glob import glob
 from collections import OrderedDict
+import time
+import requests
 
 ROOTDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATEDIR = os.path.join(ROOTDIR, 'templates')
-
 
 class BaseProject(object):
     '''
@@ -28,6 +29,11 @@ class BaseProject(object):
         'go.mod': 'go get -u all',
         'yarn.lock': 'yarn upgrade'
     }
+    
+    '''Amount of seconds to delay test execution after target_url is available.
+    This is due to the fact that some apps take longer to really deploy than what is reported back to the status check.
+    '''
+    TEST_DELAY = 10
 
     def composer_defaults(self):
         """
@@ -54,6 +60,12 @@ class BaseProject(object):
     def __init__(self, name):
         self.name = name
         self.builddir = os.path.join(TEMPLATEDIR, self.name, 'build/')
+        # Parses the github authorization token from env var by default.
+        self.github_token = os.getenv('GITHUB_TOKEN', None)
+
+       
+        # A list containing function references which will be executed against a test url, bootstrapped with a basic smoke test.
+        self.TEST_FUNCTIONS = [self.basic_smoke_test]
 
         # Include default switches on all composer commands. This can be over-ridden per-template in a subclass.
         if 'composer.json' in self.updateCommands:
@@ -127,6 +139,77 @@ class BaseProject(object):
             self.builddir)
         ]
 
+    def pull_request(self, token=None):
+        """
+        Creates a pull request from the "update" branch to master.
+        """
+        self.set_github_token(token)
+
+        authorization_header = {"Authorization": "token " + self.github_token}
+
+        pulls_api_url = 'https://api.github.com/repos/platformsh-templates/{0}/pulls'.format(self.name)
+
+        body = {"head": "update", "base": "master", "title": "Update to latest upstream"}
+        response = requests.post(pulls_api_url, headers=authorization_header, data=json.dumps(body))
+        print(response)
+        print(response.text)
+        print(response.headers)
+        return response.status_code in [201, 422]
+
+    def test(self, token=None):
+        """
+        Calls all the tests defined in self.TEST_FUNCTIONS on the test urls.
+        """
+        self.set_github_token(token)
+
+        self.results = {}
+
+        urls_to_test = self.get_test_urls()
+        if not urls_to_test:
+            print("No pull requests to test for {0}".format(self.name))
+            return
+
+        print(f"Running {len(self.TEST_FUNCTIONS)} tests.")
+        for test in self.TEST_FUNCTIONS:
+            for pull_number, url in urls_to_test.items():
+                self.results[pull_number] = test(url)
+
+        return self.results
+
+    def merge_pull_request(self, token=None):
+        """
+        Merges pull requests that pass tests.
+        """
+        self.set_github_token(token)
+        print(self.results)
+        authorization_header = {"Authorization": "token " + self.github_token}
+        pulls_api_url = 'https://api.github.com/repos/platformsh-templates/{0}/pulls'.format(self.name)
+
+        pulls = requests.get(pulls_api_url, headers=authorization_header).json()
+        responses = []
+        for pull in pulls:
+            if self.results.get(pull["number"]):
+                merge_url = f"{pulls_api_url}/{pull["number"]}/merge"
+                responses.append(requests.put(merge_url, headers=authorization_header))
+                print(f"Merged pull request {merge_url}")
+        return all(response.status_code in [200, 204] for response in responses)
+
+
+    @staticmethod
+    def basic_smoke_test(url):
+        """
+        Basic smoke test for a single PR. Only checks for a 200 OK response.
+        """
+        try:
+            response = requests.get(url)
+        except requests.exceptions.SSLError:
+            response = requests.get(url, verify=False)
+
+        if response.status_code != 200:
+            print("Test failed on {0} with code {1}".format(response.url, response.status_code))
+        return response.status_code == 200
+
+
     def package_update_actions(self):
         """
         Generates a list of package updater commands based on the updateCommands property.
@@ -156,3 +239,57 @@ class BaseProject(object):
 
         with open('{0}/composer.json'.format(self.builddir), 'w') as out:
             json.dump(composer, out, indent=2)
+
+    def get_test_urls(self):
+        """
+        Returns URLs of environments integrated to active pull requests.
+        """
+        authorization_header = {"Authorization": "token " + self.github_token}
+
+        pulls_api_url = 'https://api.github.com/repos/platformsh-templates/{0}/pulls'.format(self.name)
+        pulls = requests.get(pulls_api_url, headers=authorization_header)
+        urls = {}
+        for pull in pulls.json():
+            if "dependabot" in pull["user"]["login"]:
+                print("skipping dependabot pull request")
+                continue
+            statuses_api_url = pull["statuses_url"]
+            deployment_status = "pending"
+            while deployment_status != "success":
+                status = requests.get(statuses_api_url, headers=authorization_header)
+                if not status.json():
+                    continue
+                try:
+                    data = status.json()[0]
+                    print(data)
+                    url = data["target_url"]
+                    deployment_status = data["state"]
+
+                    if deployment_status == "failed":
+                        print("Pull request {0} was not built on Platform.sh".format(pull["url"]))
+                    if deployment_status == "success":
+                        break
+
+                except Exception as e:
+                    print("Error retrieving build status of pull request {0} on Platform.sh".format(pull["url"]))
+                    print(e)
+
+                print("Pull request {0} is still building on Platform.sh".format(pull["url"]))
+                time.sleep(10)
+            
+            print(f"Pull request {url} has finished building on Platform.sh")
+            urls[pull["number"]] = url
+        
+        # Delays execution by specified amount of seconds    
+        if urls:
+            print(f"Delaying execution of tests by {self.TEST_DELAY} seconds.")
+            time.sleep(self.TEST_DELAY)
+        return urls
+   
+    def set_github_token(self, token):
+       # CMD line token overrides env var token.
+       if token is not None:
+           self.github_token = token
+       # If token is still not set, alert user. Pd: Cannot failt gracefully as it is not on main task code.
+       if self.github_token is None:
+           print("Github token not provided. Please provide via --token argument or via GITHUB_TOKEN env var.")

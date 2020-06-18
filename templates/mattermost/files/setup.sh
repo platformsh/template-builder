42015cfa55fc855c042c2d0e58e2c1d992775e7d
Larry Garfield <larry@platform.sh>
2020-06-18 14:02:11 -0500

    Update Mattermost version.

--- templates/mattermost/files/setup.sh
+++ templates/mattermost/files/setup.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-chmod +x start.sh
-chmod +x bin/mattermost
-
-MATTERMOST_VERSION=5.21.0
-
-wget https://releases.mattermost.com/${MATTERMOST_VERSION}/mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz
+echo "Downloading Mattermost ${MATTERMOST_VERSION}"
+wget --quiet https://releases.mattermost.com/${MATTERMOST_VERSION}/mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz
 
 tar xzf mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz
 rm mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz

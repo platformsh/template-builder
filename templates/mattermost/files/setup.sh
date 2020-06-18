0b9278df095a67d5994cc58dd20ba05acff71473
Larry Garfield <larry@platform.sh>
2020-06-18 14:02:54 -0500

    Copy the default Mattermost config into place on first install.

--- templates/mattermost/files/setup.sh
+++ templates/mattermost/files/setup.sh
@@ -6,3 +6,8 @@ wget --quiet https://releases.mattermost.com/${MATTERMOST_VERSION}/mattermost-${
 tar xzf mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz
 rm mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz
 cp -a mattermost/* .
+
+chmod +x start.sh
+chmod +x bin/mattermost
+
+cp config/config.json config.default

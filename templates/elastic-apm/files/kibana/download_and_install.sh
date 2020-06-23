ad4a4b279388338f6a6ec871824a491edc957ade
Larry Garfield <larry@platform.sh>
2020-06-23 10:39:20 -0500

    Quiet wget to reduce build log noise.

--- templates/elastic-apm/files/kibana/download_and_install.sh
+++ templates/elastic-apm/files/kibana/download_and_install.sh
@@ -1,4 +1,4 @@
-wget https://artifacts.elastic.co/downloads/kibana/kibana-$KIBANA_VERSION-$KIBANA_PLATFORM.tar.gz
+wget --quiet https://artifacts.elastic.co/downloads/kibana/kibana-$KIBANA_VERSION-$KIBANA_PLATFORM.tar.gz
 shasum -a 512 kibana-$KIBANA_VERSION-$KIBANA_PLATFORM.tar.gz
 tar -xzf kibana-$KIBANA_VERSION-$KIBANA_PLATFORM.tar.gz
 rm kibana-$KIBANA_VERSION-$KIBANA_PLATFORM/config/kibana.yml

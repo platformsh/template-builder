c44b10348a304226ea336b1703ad6b1677882340
otaviojava <otaviopolianasantana@gmail.com>
2020-07-20 10:17:27 -0300

    updates prestoshop class

--- project/prestoshop.py
+++ project/prestoshop.py
@@ -5,8 +5,3 @@ class PrestaShop(RemoteProject):
     major_version = '1.7'
     remote = 'https://github.com/PrestaShop/PrestaShop.git'
 
-    @property
-    def platformify(self):
-        return super(PrestaShop, self).platformify + [
-            'cd {0} && composer require platformsh/PrestaShop-bridge'.format(self.builddir),
-        ]

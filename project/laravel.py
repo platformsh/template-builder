203588f359e0f0ee1d28cbf6851879998cababcf
Larry Garfield <larry@platform.sh>
2020-09-09 11:03:57 -0500

    Bump Laravel major version.

--- project/laravel.py
+++ project/laravel.py
@@ -2,7 +2,7 @@ from .remote import RemoteProject
 
 
 class Laravel(RemoteProject):
-    major_version = 'v7'
+    major_version = 'v8'
     remote = 'https://github.com/laravel/laravel.git'
 
     @property

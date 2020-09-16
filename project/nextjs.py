f1288db1eb8ce12f1190403869f4738f0f2f9af5
chadcarlson <chad.carlson@platform.sh>
2020-09-16 09:37:10 -0400

    Yarn updateCommands.

---
+++ project/nextjs.py
@@ -0,0 +1,9 @@
+from . import BaseProject
+import os.path
+from glob import glob
+from . import TEMPLATEDIR
+
+class Nextjs(BaseProject):
+    updateCommands = {
+        'yarn.lock': 'yarn upgrade'
+    }

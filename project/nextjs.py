5ab368f8bf2f08f0c6cb91c1a5dd99e809d928a6
chadcarlson <chad.carlson@platform.sh>
2020-09-16 09:38:53 -0400

    Unused imports.

--- project/nextjs.py
+++ project/nextjs.py
@@ -1,10 +1,6 @@
 from . import BaseProject
-import os.path
-from glob import glob
-from . import TEMPLATEDIR
 
 class Nextjs(BaseProject):
     updateCommands = {
         'yarn.lock': 'yarn upgrade'
     }
-    

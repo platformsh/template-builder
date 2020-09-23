7fc0198b03cadedece5be9182f46f8ca1045026d
chadcarlson <chad.carlson@platform.sh>
2020-09-23 12:29:52 -0400

    Fix strapi.

---
+++ templates/strapi/files/backend/platformsh/server.js
@@ -0,0 +1,30 @@
+module.exports = ({ env }) => ({
+
+    // Basic server settings.
+    // 
+    // See https://strapi.io/documentation/v3.x/getting-started/deployment.html#application-configuration
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+
+    // Admin user JWT configuration.
+    // 
+    // See https://strapi.io/documentation/v3.x/plugins/users-permissions.html#jwt-configuration
+    admin: {
+        auth: {
+          secret: env('PLATFORM_PROJECT_ENTROPY'),
+        },
+    },
+    // GraphQL endpoint configuration.
+    // 
+    // See https://strapi.io/documentation/v3.x/plugins/graphql.html#usage
+    graphql: {
+        endpoint: '/graphql',
+        shadowCRUD: true,
+        playgroundAlways: false,
+        depthLimit: 7,
+        amountLimit: 100,
+        apolloServer: {
+            tracing: false,
+        }
+    },
+});

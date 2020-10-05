7f0e1f3ed67a441adc7b105164264975c05ef97a
chadcarlson <chad.carlson@platform.sh>
2020-10-05 09:50:05 -0400

    Feedback on deps.

--- project/wordpress.py
+++ project/wordpress.py
@@ -40,15 +40,5 @@ class Wordpress(RemoteProject):
         return super(Wordpress, self).platformify + [
             (self.modify_composer, [wp_modify_composer]),
             'cd {0} && composer update --ignore-platform-reqs'.format(self.builddir),
-            'cd {0} && composer require platformsh/config-reader --ignore-platform-reqs'.format(self.builddir),
-            # WP-CLI Composer recommendations (https://make.wordpress.org/cli/handbook/guides/installing/#installing-via-composer).
-            'cd {0} && composer require wp-cli/wp-cli-bundle'.format(self.builddir),
-            'cd {0} && composer require symfony/event-dispatcher'.format(self.builddir),
-            'cd {0} && composer require symfony/lock'.format(self.builddir),
-            'cd {0} && composer require illuminate/view'.format(self.builddir),
-            'cd {0} && composer require symfony/yaml'.format(self.builddir),
-            'cd {0} && composer require psy/psysh'.format(self.builddir),
-            'cd {0} && composer require symfony/dependency-injection'.format(self.builddir),
-            'cd {0} && composer require symfony/dependency-injection'.format(self.builddir),
-            
+            'cd {0} && composer require platformsh/config-reader wp-cli/wp-cli-bundle psy/psysh --ignore-platform-reqs'.format(self.builddir),
         ]

2548e4b22c8b3c1a17d2dcd095c8bd51542189c0
chadcarlson <chad.carlson@platform.sh>
2020-12-10 17:20:53 -0500

    cleanup vanilla.

--- templates/wordpress-vanilla/files/wp-config.php
+++ templates/wordpress-vanilla/files/wp-config.php
@@ -13,18 +13,10 @@ if (isset($_SERVER['HTTP_HOST'])) {
 if (getenv('PLATFORM_RELATIONSHIPS')) {
 	if(getenv('DB_NAME')){
 
-// if ($config->isValidPlatform()) {
-// 	if ($config->hasRelationship('database')) {
-		// This is where we get the relationships of our application dynamically
-		// from Platform.sh.
-
 		// Avoid PHP notices on CLI requests.
 		if (php_sapi_name() === 'cli') {
 			session_save_path("/tmp");
 		}
-
-		// Get the database credentials
-		// $credentials = $config->credentials('database');
 
 		// We are using the first relationship called "database" found in your
 		// relationships. Note that you can call this relationship as you wish
@@ -35,27 +27,6 @@ if (getenv('PLATFORM_RELATIONSHIPS')) {
 		define( 'DB_HOST', getenv('DB_HOST'));
 		define( 'DB_CHARSET', 'utf8' );
 		define( 'DB_COLLATE', '' );
-
-		// Check whether a route is defined for this application in the Platform.sh
-		// routes. Use it as the site hostname if so (it is not ideal to trust HTTP_HOST).
-		// if (getenv('WP_HOME')) {
-		// // if ($config->routes()) {
-
-		// 	$routes = $config->routes();
-
-		// 	foreach ($routes as $url => $route) {
-		// 		if ($route['type'] === 'upstream' && $route['upstream'] === $config->applicationName) {
-
-		// 			// Pick the first hostname, or the first HTTPS hostname if one exists.
-		// 			$host = parse_url($url, PHP_URL_HOST);
-		// 			$scheme = parse_url($url, PHP_URL_SCHEME);
-		// 			if ($host !== false && (!isset($site_host) || ($site_scheme === 'http' && $scheme === 'https'))) {
-		// 				$site_host = $host;
-		// 				$site_scheme = $scheme ?: 'http';
-		// 			}
-		// 		}
-		// 	}
-		// }
 
 		// Debug mode should be disabled on Platform.sh. Set this constant to true
 		// in a wp-config-local.php file to skip this setting on local development.

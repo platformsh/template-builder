6aad12532b00a4b3c29f78c5ae41f21a00b526a6
Laurent Arnoud <laurent.arnoud@platform.sh>
2020-12-11 15:41:17 +0100

    Update to Rails 6.1.0 and enable bundle deployment

--- project/rails.py
+++ project/rails.py
@@ -16,11 +16,9 @@ class Rails(BaseProject):
     @property
     def platformify(self):
         return super(Rails, self).platformify + [
-            'cd {0} && bundle add unicorn pg platform_sh_rails --group "production" --skip-install'.format(self.builddir),
-            'cd {0} && echo config/database.yml >> .gitignore'.format(self.builddir),
-            'cd {0} && mv config/database.yml config/database.yml.example'.format(self.builddir),
+            'cd {0} && if ! grep -q platform_sh_rails Gemfile; then bundle add unicorn pg platform_sh_rails --group "production"; fi;'.format(self.builddir),
             # Remove the Rails Ruby version lock file, as it's not needed.
-            'cd {0} && rm .ruby-version'.format(self.builddir),
+            'cd {0} && rm -f .ruby-version'.format(self.builddir),
             # Remove the Ruby version from the Gemfile, as it pins to a .z release which is too strict.
             "cd {0} && sed '/^ruby /d' Gemfile > temp && mv temp Gemfile".format(self.builddir),
             # Make bootsnap use the /tmp folder

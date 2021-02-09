174abb8770df347a03e99a2da8c7906f96a19972
Larry Garfield <larry@platform.sh>
2021-02-09 14:52:59 -0600

    Update comment.

--- project/akeneo.py
+++ project/akeneo.py
@@ -12,10 +12,9 @@ class Akeneo(RemoteProject):
         def akeneo_modify_composer(composer):
             """
             akeneo/pim-community-standard requires PHP 7.4, but it's dependency ocramius/package-versions has a complex
-            requirement set.  2.1.0 is the only version I could find that works on Composer 2 and PHP 7.4, but it needs
-            7.4.7 specifically and a platform.php value of just 7.4 isn't enough.  This combination seems to work.
-            Akeneo doesn't work with PHP 8 yet, because it depends on a version of Doctrine that is pinned to only
-            support PHP 7, despite Doctrine and package-versions being written by the same person.
+            requirement set.  1.5.1 is the only version I could find that works on Composer 2 and PHP 7.4, but it needs
+            to be faked out with a platform version of 7.3 despite working on 7.4.  This is what happens when packages
+            are needlessly and pointlessly aggressive in dropping old version support.
             """
 
             composer['require']['ocramius/package-versions'] = "1.5.1"

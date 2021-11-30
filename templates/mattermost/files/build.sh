9c75a159c57ad8cabf8dadddee4de06c4e46a6d3
chadcarlson <chad.carlson@platform.sh>
2021-11-30 16:51:47 -0500

    Fix MM. Try gatsby+strapi.

--- templates/mattermost/files/build.sh
+++ templates/mattermost/files/build.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/env bash
 
-MATTERMOST_VERSION=$(cat mattermost_version)
+export MATTERMOST_VERSION=$(cat mattermost_version)
 
-echo "Downloading Mattermost ${MATTERMOST_VERSION}"
-wget --quiet -c https://releases.mattermost.com/${MATTERMOST_VERSION}/mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz -O - | tar -xz
-cp -a mattermost/* .
+download_mattermost() {
+    printf "\n  ✔ \033[1mDownloading Mattermost...\033[0m ($MATTERMOST_VERSION)\n\n"
+    wget --quiet -c https://releases.mattermost.com/${MATTERMOST_VERSION}/mattermost-${MATTERMOST_VERSION}-linux-amd64.tar.gz -O - | tar -xz
+    cp -a mattermost/* .
+    chmod +x bin/mattermost
+}
 
-chmod +x start.sh
-chmod +x bin/mattermost
+set_config() {
+    cp config/config.json config.default
+}
 
-cp config/config.json config.default
+set -e
+download_mattermost
+set_config
--- templates/mattermost/files/deploy.sh
+++ templates/mattermost/files/deploy.sh
@@ -1,5 +1,30 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [ ! -f config/config.json ] || [ ! -s config/config.json ]; then
-    cp config.default config/config.json
-fi
+set_config() {
+    if [ ! -f config/config.json ] || [ ! -s config/config.json ]; then
+        cp config.default config/config.json
+    fi  
+}
+
+first_deploy() {
+
+    # Demo details - currently blocked by upstream issue (version mismatch - https://github.com/mattermost/mattermost-server/issues/19023).
+    printf "\n\033[1mInitializing Mattermost on first deploy...\033[0m\n"
+    printf "\n  ✔ \033[1mCreating initial admin user\033[0m ($PSH_INITADMIN_USERNAME/$PSH_INITADMIN_EMAIL/$PSH_INITADMIN_PASSWORD)\n      "
+    ./bin/mmctl user create --local --username $PSH_INITADMIN_USERNAME --email $PSH_INITADMIN_EMAIL --password $PSH_INITADMIN_PASSWORD
+    printf "\n  ✔ \033[1mCreating initial private team\033[0m ($PSH_FIRSTTEAM_NAME/$PSH_FIRSTTEAM_DISPLAYNAME)\n    "
+    ./bin/mmctl team create --local --name $PSH_FIRSTTEAM_NAME --display-name $PSH_FIRSTTEAM_DISPLAYNAME --private
+    printf "\n  ✔ \033[1mCreating initial channel\033[0m ($PSH_FIRSTCHANNEL_NAME/$PSH_FIRSTCHANNEL_DISPLAYNAME)\n    "
+    ./bin/mmctl channel create --local --team $PSH_FIRSTTEAM_NAME --name $PSH_FIRSTCHANNEL_NAME --display-name $PSH_FIRSTCHANNEL_DISPLAYNAME
+    printf "\n  ✔ \033[1mPosting welcome/warning messages to channel...\033[0m\n    "
+    ./bin/mmctl post create --local $PSH_FIRSTTEAM_NAME:$PSH_FIRSTCHANNEL_NAME --message $PSH_WELCOME_MESSAGE
+    ./bin/mmctl post create --local $PSH_FIRSTTEAM_NAME:$PSH_FIRSTCHANNEL_NAME --message "$PSH_WARNING_MESSAGE1"
+    ./bin/mmctl post create --local $PSH_FIRSTTEAM_NAME:$PSH_FIRSTCHANNEL_NAME --message "$PSH_WARNING_MESSAGE2"
+    printf "\n\n\033[1m$PSH_WELCOME_MESSAGE\033[0m"
+    printf "\n\033[1m$PSH_WARNING_MESSAGE1\033[0m"
+    printf "\n\033[1m$PSH_WARNING_MESSAGE2\033[0m\n\n"
+    # Keep track of first deploy.
+    # touch /app/.config/platformsh.installed
+}
+
+set_config

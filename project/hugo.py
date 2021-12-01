ba61ee66e074c68d3620a972a46716fd825dd248
chadcarlson <chad.carlson@platform.sh>
2021-12-01 08:50:25 -0500

    Update upstream handling.

--- project/hugo.py
+++ project/hugo.py
@@ -7,9 +7,12 @@ class Hugo(BaseProject):
     @property
     def platformify(self):
 
+        major_version = "0.89"
+
         response = requests.get('https://api.github.com/repos/gohugoio/hugo/releases')
-        version = response.json()[0]["name"][1:]
+
+        tags = [release["tag_name"] for release in response.json() if release["tag_name"].startswith("v{}".format(major_version)) and 'beta' not in release["tag_name"] and 'alpha' not in release["tag_name"]]
 
         return super(Hugo, self).platformify + [
-            'cd {0} && echo {1} > hugo_version'.format(self.builddir, version),
+            'cd {0} && echo {1} > hugo_version'.format(self.builddir, tags[0]),
         ]

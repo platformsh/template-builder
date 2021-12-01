ac258505eef5bd69ac34d7c3d01a418d1d8e20be
chadcarlson <chad.carlson@platform.sh>
2021-12-01 09:07:19 -0500

    Fix hugo + mattermost.

--- project/hugo.py
+++ project/hugo.py
@@ -14,5 +14,5 @@ class Hugo(BaseProject):
         tags = [release["tag_name"] for release in response.json() if release["tag_name"].startswith("v{}".format(major_version)) and 'beta' not in release["tag_name"] and 'alpha' not in release["tag_name"]]
 
         return super(Hugo, self).platformify + [
-            'cd {0} && echo {1} > hugo_version'.format(self.builddir, tags[0]),
+            'cd {0} && echo {1} > hugo_version'.format(self.builddir, tags[0][1:]),
         ]
--- project/mattermost.py
+++ project/mattermost.py
@@ -14,5 +14,5 @@ class Mattermost(BaseProject):
         tags = [release["tag_name"] for release in response.json() if release["tag_name"].startswith("v{}".format(major_version)) and 'beta' not in release["tag_name"] and 'alpha' not in release["tag_name"]]
 
         return super(Mattermost, self).platformify + [
-            'cd {0} && echo {1} > mattermost_version'.format(self.builddir, tags[0]),
+            'cd {0} && echo {1} > mattermost_version'.format(self.builddir, tags[0][1:]),
         ]

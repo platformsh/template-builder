d994a7b73d22deae9ae5e490e705d6df157eef70
chadcarlson <chad.carlson@platform.sh>
2022-02-23 12:15:48 -0500

    Don't collide with upstream workflows.

--- project/sylius.py
+++ project/sylius.py
@@ -4,10 +4,8 @@ class Sylius(RemoteProject):
     major_version = 'v1.11'
     remote = 'https://github.com/Sylius/Sylius-Standard.git'
 
-    # @property
-    # def platformify(self):
-    #     return super(Sylius, self).platformify + [
-    #         # 'cd {0} && composer update -W'.format(self.builddir) + self.composer_defaults()
-    #         'cd {0} && composer require platformsh/config-reader drush/drush drupal/console drupal/redis'.format(self.builddir)  + self.composer_defaults(),
-    #         # 'cd {0} && composer update -W'.format(self.builddir) + self.composer_defaults()
-    #     ]
+    @property
+    def platformify(self):
+        return super(Sylius, self).platformify + [
+            'cd {0} && rm -rf .github',
+        ]

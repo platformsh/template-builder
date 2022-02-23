77696185d08c110ab1ae9e452a0c605911d487ae
chadcarlson <chad.carlson@platform.sh>
2022-02-23 10:42:40 -0500

    nuxt + magento.

--- project/magento.py
+++ project/magento.py
@@ -2,5 +2,5 @@ from .remote import RemoteProject
 
 
 class Magento2ce(RemoteProject):
-    major_version = '2.3'
+    major_version = '2.4'
     remote = 'https://github.com/magento/magento2.git'
--- project/nuxtjs.py
+++ project/nuxtjs.py
@@ -1,5 +1,6 @@
 from . import BaseProject
 import json
+import os
 
 class Nuxtjs(BaseProject):
 
@@ -10,6 +11,9 @@ class Nuxtjs(BaseProject):
 
     @property
     def update(self):
+
+        ROOTDIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+        TEMPLATEDIR = os.path.join(ROOTDIR, 'templates/nuxtjs')
 
         projectName = "nuxtjs-platformsh"
         # create-nuxt-app uses an interactive prompt to select different project options.
@@ -39,12 +43,12 @@ class Nuxtjs(BaseProject):
             # Create a NuxtJS app using Yarn, since there's no dedicated upstream repo for it. Yarn prevents you
             # from creating a new project in a nonempty dir, so the project is made in projectName before its
             # contents are copied into builddir.
-            'cd {0} && npx create-nuxt-app --answers \"{1}\" {2}'.format(self.builddir, json.dumps(createNuxtAppAnswers, default=set_default).replace('"', '\\"'), projectName),
-            'cd {0}/{1} && npx nuxt telemetry disable'.format(self.builddir, projectName),
-            'cd {0} && rm -rf {1}/.git'.format(self.builddir, projectName),
-            'cd {0} && cp -r {1}/ {0}'.format(self.builddir, projectName),
-            'cd {0} && rm -rf {1}'.format(self.builddir, projectName),
+            'cd {0} && npx create-nuxt-app --answers \"{1}\" {2}'.format(TEMPLATEDIR, json.dumps(createNuxtAppAnswers, default=set_default).replace('"', '\\"'), projectName),
             # Disable telemtery so we are not asked for it when running the application the first time
+            'cd {0}/{1} && npx nuxt telemetry disable'.format(TEMPLATEDIR, projectName),
+            'cd {0} && rm -rf {1}/.git'.format(TEMPLATEDIR, projectName),
+            'cd {0} && cp -r {1}/{2}/* .'.format(self.builddir, TEMPLATEDIR, projectName)
+            'cd {0} && rm -rf {1}'.format(TEMPLATEDIR, projectName),
         ] + super(Nuxtjs, self).update
 
     @property

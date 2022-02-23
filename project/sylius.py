6bd094cfe03920a6de171f5e8df35bda8a9f0272
chadcarlson <chad.carlson@platform.sh>
2022-02-23 12:04:43 -0500

    Test sylius.

---
+++ project/sylius.py
@@ -0,0 +1,13 @@
+from .remote import RemoteProject
+
+class Sylius(RemoteProject):
+    major_version = '1.11'
+    remote = 'https://github.com/Sylius/Sylius-Standard.git'
+
+    @property
+    def platformify(self):
+        return super(Drupal8, self).platformify + [
+            # 'cd {0} && composer update -W'.format(self.builddir) + self.composer_defaults()
+            'cd {0} && composer require platformsh/config-reader drush/drush drupal/console drupal/redis'.format(self.builddir)  + self.composer_defaults(),
+            # 'cd {0} && composer update -W'.format(self.builddir) + self.composer_defaults()
+        ]

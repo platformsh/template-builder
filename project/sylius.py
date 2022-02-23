e82cfbefdb59e643b76645b9ca4781929c60ffd1
chadcarlson <chad.carlson@platform.sh>
2022-02-23 12:08:54 -0500

    Cleanup.

--- project/sylius.py
+++ project/sylius.py
@@ -4,10 +4,10 @@ class Sylius(RemoteProject):
     major_version = '1.11'
     remote = 'https://github.com/Sylius/Sylius-Standard.git'
 
-    @property
-    def platformify(self):
-        return super(Drupal8, self).platformify + [
-            # 'cd {0} && composer update -W'.format(self.builddir) + self.composer_defaults()
-            'cd {0} && composer require platformsh/config-reader drush/drush drupal/console drupal/redis'.format(self.builddir)  + self.composer_defaults(),
-            # 'cd {0} && composer update -W'.format(self.builddir) + self.composer_defaults()
-        ]
+    # @property
+    # def platformify(self):
+    #     return super(Sylius, self).platformify + [
+    #         # 'cd {0} && composer update -W'.format(self.builddir) + self.composer_defaults()
+    #         'cd {0} && composer require platformsh/config-reader drush/drush drupal/console drupal/redis'.format(self.builddir)  + self.composer_defaults(),
+    #         # 'cd {0} && composer update -W'.format(self.builddir) + self.composer_defaults()
+    #     ]

00433051345340fbce1cf07ff9376d7e6d5a736a
chadcarlson <chad.carlson@platform.sh>
2022-03-14 14:28:47 -0400

    Update -w.

--- project/drupal.py
+++ project/drupal.py
@@ -70,5 +70,5 @@ class Drupal8_govcms8(RemoteProject):
            # For now, just skip installing console on GovCMS. I don't know if anyone uses it anyway.
         #    'cd {0} && composer require platformsh/config-reader drush/drush drupal/redis'.format(self.builddir) + self.composer_defaults(),
            'cd {0} && composer require platformsh/config-reader drush/drush drupal/redis'.format(self.builddir) + self.composer_defaults(),
-        #    'cd {0} && composer update -W'.format(self.builddir) + self.composer_defaults(),
+           'cd {0} && composer update -W'.format(self.builddir) + self.composer_defaults(),
         ]

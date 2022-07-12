8bcf3024c96e3b36e9357e84f99ccd1bf8cb87cd
Paul Gilzow <paul.gilzow@platform.sh>
2022-07-12 14:48:25 -0500

    Adds ocramius/package-versions to allow-plugins for pimcore

--- project/pimcore.py
+++ project/pimcore.py
@@ -9,4 +9,5 @@ class Pimcore(RemoteProject):
     def platformify(self):
         return super(Pimcore, self).platformify + [
             'cd {0} && composer update --no-scripts && composer require platformsh/config-reader doctrine/orm  --no-scripts'.format(self.builddir) + self.composer_defaults(),
+            'cd {0} && composer config allow-plugins.ocramius/package-versions true --no-plugins'.format(self.builddir),
         ]

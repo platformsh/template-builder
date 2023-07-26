18554a5ee075a3e6f2812dab55dd8444cceb4b02
Paul Gilzow <paul.gilzow@platform.sh>
2023-07-26 15:19:04 -0500

    updates nuxt class to use the new v3 way of creating a nuxt app. Adds env var to config to disable telemetry

--- project/nuxtjs.py
+++ project/nuxtjs.py
@@ -41,16 +41,19 @@ class Nuxtjs(BaseProject, ReadmeMixin):
             raise TypeError
 
         actions = [
+            #just in case a previous update failed, our temp directory may still be around
+            'cd {0} && if [ -d "{1}" ]; then echo "removing previous {1} directory...";rm -rf {1};fi;'.format(TEMPLATEDIR,projectName),
             super(Nuxtjs, self).rename_psh_readme(),
             # Create a NuxtJS app using Yarn, since there's no dedicated upstream repo for it. Yarn prevents you
             # from creating a new project in a nonempty dir, so the project is made in projectName before its
             # contents are copied into builddir.
-            'cd {0} && npx create-nuxt-app --answers \"{1}\" {2}'.format(TEMPLATEDIR,
-                                                                         json.dumps(createNuxtAppAnswers,
-                                                                                    default=set_default).replace(
-                                                                             '"', '\\"'), projectName),
+            # 'cd {0} && npx create-nuxt-app --answers \"{1}\" {2}'.format(TEMPLATEDIR,
+            #                                                              json.dumps(createNuxtAppAnswers,
+            #                                                                         default=set_default).replace(
+            #                                                                  '"', '\\"'), projectName),
+            'cd {0} && npx nuxi@latest init {1}'.format(TEMPLATEDIR,projectName),
             # Disable telemtery so we are not asked for it when running the application the first time
-            'cd {0}/{1} && npx nuxt telemetry disable'.format(TEMPLATEDIR, projectName),
+            #'cd {0}/{1} && npx nuxt telemetry disable'.format(TEMPLATEDIR, projectName),
             'cd {0} && rm -rf {1}/.git'.format(TEMPLATEDIR, projectName),
             'cd {0} && cp -r {1}/{2}/* .'.format(self.builddir, TEMPLATEDIR, projectName),
             'cd {0} && rm -rf {1}'.format(TEMPLATEDIR, projectName),

f9d8aaf10ce283a055beddf92b5c516c01c99670
Paul Gilzow <paul.gilzow@platform.sh>
2023-07-27 10:56:35 -0500

    renames magento.py to magento-archived.py as mangento no longer needs anything beyond what is done in BaseProject Removes the import of the magento class in the baseproject file

--- dodo.py
+++ dodo.py
@@ -16,10 +16,10 @@ from project.elasticapm import Elastic_apm
 from project.gatsby import Gatsby
 from project.hugo import Hugo
 from project.laravel import Laravel
-from project.magento import Magento2ce
+# from project.magento import Magento2ce
 from project.pimcore import Pimcore
 from project.laravel import Laravel
-from project.magento import Magento2ce
+# from project.magento import Magento2ce
 from project.mattermost import Mattermost
 from project.mautic import Mautic
 from project.nextjs import Nextjs
